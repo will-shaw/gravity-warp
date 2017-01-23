4392216b6ec28e1452324314f33ee93876570ad3
Will Shaw <will_shaw@live.com>
2017-01-23 14:02:08 +1300

    Added a solid box. Updated gravity script to use an array for easier testing of multiple objects.

--- Assets/Scripts/GravityWarp.cs
+++ Assets/Scripts/GravityWarp.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GravityWarp : MonoBehaviour {
 
 	float gravityScale = 4.0f;
 
-	public Transform box;
+	public Text text;
+
+	public Transform[] boxes;
 
 	public float thrust;
 
@@ -17,39 +18,42 @@ public class GravityWarp : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.UpArrow) || gravity == "U") {
-			box.GetComponent<Rigidbody2D> ().gravityScale = -gravityScale;
-			gravity = "U";
-		}
-		if (Input.GetKey (KeyCode.DownArrow) || gravity == "D") {
-			box.GetComponent<Rigidbody2D> ().gravityScale = gravityScale;
-			gravity = "D";
-		}
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			box.GetComponent<Rigidbody2D> ().gravityScale = 0;
-			gravity = "R";
-		}
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			box.GetComponent<Rigidbody2D> ().gravityScale = 0;
-			gravity = "L";
-		}
-		if (Input.GetKey (KeyCode.Space)) {
-			if (gravity == "0") {
-				gravity = lastGravDir;
-			} else {
-				lastGravDir = gravity;
-				gravity = "0";
-				box.GetComponent<Rigidbody2D> ().gravityScale = 0;
+		text.text = boxes[0].GetComponent<Rigidbody2D>().velocity.ToString();
+		for (int i = 0; i < boxes.Length; i++) {
+			if (Input.GetKey(KeyCode.UpArrow) || gravity == "U") {
+				boxes[i].GetComponent<Rigidbody2D> ().gravityScale = -gravityScale;
+				gravity = "U";
 			}
-		}
-		if (gravity == "L") {
-			box.GetComponent<Rigidbody2D> ().AddForce(new Vector2(-box.position.x * thrust, 0));
-		}
-		if (gravity == "R") {
-			box.GetComponent<Rigidbody2D> ().AddForce(new Vector2(box.position.x * thrust, 0));
+			if (Input.GetKey (KeyCode.DownArrow) || gravity == "D") {
+				boxes[i].GetComponent<Rigidbody2D> ().gravityScale = gravityScale;
+				gravity = "D";
+			}
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				boxes[i].GetComponent<Rigidbody2D> ().gravityScale = 0;
+				gravity = "R";
+			}
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				boxes[i].GetComponent<Rigidbody2D> ().gravityScale = 0;
+				gravity = "L";
+			}
+			if (Input.GetKey (KeyCode.Space)) {
+				if (gravity == "0") {
+					gravity = lastGravDir;
+				} else {
+					lastGravDir = gravity;
+					gravity = "0";
+					boxes[i].GetComponent<Rigidbody2D> ().gravityScale = 0;
+				}
+			}
+			if (gravity == "L") {
+				boxes[i].GetComponent<Rigidbody2D> ().AddForce(new Vector2(4.0f * thrust, 0));
+			}
+			if (gravity == "R") {
+				boxes[i].GetComponent<Rigidbody2D> ().AddForce(new Vector2(-4.0f * thrust, 0));
+			}
 		}
 	}
 }
