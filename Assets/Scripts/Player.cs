067c8798550ebf83ecb8b194922cac3e76ffe1cf
will-shaw <will_shaw@live.com>
2017-01-25 11:21:50 +1300

    Art design prototype. Also shows simple camera tracking.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Transform player;
+
+	public float speed = 2.0f;
+
+	// Update is called once per frame
+	void Update () {
+		float movementInput = Input.GetAxis("Horizontal");
+
+		player.Translate( new Vector3(Time.deltaTime * speed * movementInput,0,0), Space.World);
+		Camera.main.GetComponent<Transform>().position = new Vector3 (player.position.x, player.position.y, -10); // Camera follows the player with specified offset position
+	}
+}
