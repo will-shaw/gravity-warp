883241ddbb2cb9c554ae859874340a4ff0573a45
Will Shaw <will_shaw@live.com>
2017-01-24 16:31:30 +1300

    Added gravity color scroll selection. Added camera zoom.

--- Assets/Scripts/wil_proto/MouseMenu.cs
+++ Assets/Scripts/wil_proto/MouseMenu.cs
@@ -1,59 +1,111 @@
 ﻿using UnityEngine;
 
-public class MouseMenu : MonoBehaviour {
+public class MouseMenu : MonoBehaviour
+{
 
-	public float threshhold = 0.3f;
+    public float threshhold = 0.3f;
 
-	public float distance = 300f;
+    public float distance = 300f;
 
-	public Transform menu;
-	Transform instantiatedMenu;
-	float x;
-	float y;
+    public Transform indicator;
 
-    void OnMouseDown() {
-        // Instantiate prefab for visual element of menu.
-		instantiatedMenu = Instantiate(menu);
+    Color[] colors = { Color.red, Color.green, Color.blue, Color.yellow };
 
-		this.x = Input.mousePosition.x;
-		this.y = Input.mousePosition.y;
+    int gColorIndex = 0;
 
-    }
+    public Transform menu;
+    Transform instantiatedMenu;
+    float x;
+    float y;
 
-	void OnMouseUp() {
-        // Instantiate prefab for visual element of menu.
-
-		float x = Input.mousePosition.x;
-		float y = Input.mousePosition.y;
-
-		float dx = this.x - x;
-		float dy = this.y - y;
-
-		if (dx > distance && (dy < Deadzone(dx, 0) && dy > Deadzone(dx, 1))) {
-			print("Gravity Left");
-			GravityWarp.gravity = "L";
-		} else if (dx < -distance && (dy < Deadzone(dx, 1) && dy > Deadzone(dx, 0))) {
-			print("Gravity Right");
-			GravityWarp.gravity = "R";			
-		} else if (dy > distance && (dx < Deadzone(dy, 0) && dx > Deadzone(dy, 1))) {
-			print("Gavity Down");
-			GravityWarp.gravity = "D";			
-		} else if (dy < -distance && (dx < Deadzone(dy, 1) && dx > Deadzone(dy, 0))) {
-			print("Gravity Up");
-			GravityWarp.gravity = "U";
-		}
-
-		Destroy (instantiatedMenu.gameObject);
-		
-    }
-
-	float Deadzone(float v, int s) {
-		if (s == 0) { 
-			return v * threshhold;
-		} else {
-			return v * -threshhold;
-		}
+	void Start() {
+        indicator.GetComponent<SpriteRenderer>().color = colors[0];
 	}
+
+    void OnMouseDown()
+    {
+
+        this.x = Input.mousePosition.x;
+        this.y = Input.mousePosition.y;
+
+        // Instantiate the radial menu at the mouse position.
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 4.0f;
+        Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+        instantiatedMenu = Instantiate(menu, objectPos, Quaternion.identity);
+
+    }
+
+    void OnMouseUp()
+    {
+
+        float x = Input.mousePosition.x;
+        float y = Input.mousePosition.y;
+
+        float dx = this.x - x;
+        float dy = this.y - y;
+
+        if (dx > distance && (dy < Deadzone(dx, 0) && dy > Deadzone(dx, 1)))
+        {
+            GravityWarp.gravity = "L";
+        }
+        else if (dx < -distance && (dy < Deadzone(dx, 1) && dy > Deadzone(dx, 0)))
+        {
+            GravityWarp.gravity = "R";
+        }
+        else if (dy > distance && (dx < Deadzone(dy, 0) && dx > Deadzone(dy, 1)))
+        {
+            GravityWarp.gravity = "D";
+        }
+        else if (dy < -distance && (dx < Deadzone(dy, 1) && dx > Deadzone(dy, 0)))
+        {
+            GravityWarp.gravity = "U";
+        }
+
+        Destroy(instantiatedMenu.gameObject);
+
+    }
+
+    void Update()
+    {
+        var d = Input.GetAxis("Mouse ScrollWheel");
+        if (d > 0f)
+        {
+            if (gColorIndex > 0)
+            {
+                indicator.GetComponent<SpriteRenderer>().color = colors[--gColorIndex];
+            }
+            else
+            {
+                gColorIndex = 3;
+                indicator.GetComponent<SpriteRenderer>().color = colors[gColorIndex];
+            }
+        }
+        else if (d < 0f)
+        {
+            if (gColorIndex < 3)
+            {
+                indicator.GetComponent<SpriteRenderer>().color = colors[++gColorIndex];
+            }
+            else
+            {
+                gColorIndex = 0;
+                indicator.GetComponent<SpriteRenderer>().color = colors[gColorIndex];
+            }
+        }
+    }
+
+    float Deadzone(float v, int s)
+    {
+        if (s == 0)
+        {
+            return v * threshhold;
+        }
+        else
+        {
+            return v * -threshhold;
+        }
+    }
 
 
 }
