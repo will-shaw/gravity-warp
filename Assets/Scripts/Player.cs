89ea6664a424d475a792bec79ab42de9a8a56b14
will-shaw <will_shaw@live.com>
2017-01-26 12:35:48 +1300

    Added background artwork. Added parallax prototype on backround artwork. Implemented prototype jumping and movement using physics. Updated camera tracking (still simple).

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-	public Transform player;
+    public Transform bg;
+    public Transform player;
 
-	public float speed = 2.0f;
+    public float speed = 2.0f;
 
-	// Update is called once per frame
-	void Update () {
-		float movementInput = Input.GetAxis("Horizontal");
+    float cooldown = 0;
 
-		player.Translate( new Vector3(Time.deltaTime * speed * movementInput,0,0), Space.World);
-		Camera.main.GetComponent<Transform>().position = new Vector3 (player.position.x, player.position.y, -10); // Camera follows the player with specified offset position
-	}
+    // Update is called once per frame
+
+    void Update()
+    {
+		float movementInput = 0;
+        if (Time.realtimeSinceStartup > cooldown + 1)
+        {
+            movementInput = Input.GetAxis("Horizontal");
+        }
+	
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			player.GetComponent<Rigidbody2D>().AddForce(new Vector2(Time.deltaTime * 250f, 0));			
+		} else if (Input.GetKey(KeyCode.LeftArrow)) {
+			player.GetComponent<Rigidbody2D>().AddForce(new Vector2(Time.deltaTime * -250f, 0));						
+		}
+
+		
+        //player.Translate(new Vector3(Time.deltaTime * speed * movementInput, 0, 0), Space.World);
+        Camera.main.GetComponent<Transform>().position = new Vector3(player.position.x, player.position.y + 2.54f, -10); // Camera follows the player with specified offset position
+        bg.position = new Vector3(player.position.x * 0.5f, 0, 2); // Camera follows the player with specified offset position
+
+        if (Input.GetKey(KeyCode.UpArrow) && Time.realtimeSinceStartup > cooldown + 1)
+        {
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 250.0f));
+            cooldown = Time.realtimeSinceStartup;
+        }
+
+    }
 }
