39a3bec3c4f79fafe51d11e4a8b488cd71074757
will-shaw <will_shaw@live.com>
2017-01-29 23:21:05 +1300

    Updated CameraZoom Event Tracking. Disabled simple camera tracking in Player

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -5,60 +5,69 @@ public class CameraZoom : MonoBehaviour
     Transform cam; // Transform component of the main camera.
     public Transform player;
     public Vector3 levelCenter; // Centerpoint of the entire level.
-    public float maxZoom; // Most zoomed in camera size.
-    public float minZoom; // Least zoomed in camera size.
+    public float zoomFar; // Most zoomed OUT camera size.
+    public float zoomClose; // Most zoomed IN camera size.
     public float zoomSpeed; // Speed at which the camera moves.
 
     /* Static variables */
     static Vector3 eventCenter; // Centerpoint of event to move to.
-    static int moving = 0; // State of the movement.
+    static int move = 0; // State of the movement.
 
     void Start()
     {
         cam = Camera.main.GetComponent<Transform>();
+        Camera.main.orthographicSize = zoomClose;
     }
 
     void Update()
     {
-
-        if (Camera.main.orthographicSize == maxZoom && Input.GetKeyDown(KeyCode.Tab))
+        // If camera is currently zoomed out, pressing tab should zoom in.
+        if (Camera.main.orthographicSize == zoomFar && Input.GetKeyDown(KeyCode.Tab))
         {
-            moving = -1;
+            move = -1;
+        }
+        // If camera is currently zoomed in, pressing tab should zoom out.
+        if (Camera.main.orthographicSize == zoomClose && Input.GetKeyDown(KeyCode.Tab))
+        {
+            move = 1;
         }
 
-        if (Camera.main.orthographicSize == minZoom && Input.GetKeyDown(KeyCode.Tab))
+        // If player is null, stop trying to modify the camera at all.
+        if (player != null && move != 0)
         {
-            moving = 1;
-        }
+            switch (move)
+            {
+                case -1:
+                    Zoom(zoomClose);
+                    Move(player.position.x, player.position.y, cam.position.z);
+                    break;
+                case 1:
+                    Zoom(zoomFar);
+                    Move(levelCenter.x, levelCenter.y, cam.position.z);
+                    break;
+                case 8:
+                    Zoom(zoomClose);
+                    Move(eventCenter.x, eventCenter.y, cam.position.z);
+                    break;
+                default:
+                    break;
+            }
 
-        switch (moving)
-        {
-            case -1:
-                Zoom(minZoom);
-                Move(player.position.x, player.position.y, cam.position.z);
-                break;
-            case 1:
-                Zoom(maxZoom);
-                Move(player.position.x, player.position.y, cam.position.z);
-                break;
-            case 8:
-                print("Moving to event center");
-                break;
-            default:
-                break;
+            if ((Camera.main.orthographicSize == zoomClose || Camera.main.orthographicSize == zoomFar) &&
+                        ((cam.position.x == player.position.x && cam.position.x == player.position.x) ||
+                        cam.position == levelCenter || cam.position == eventCenter))
+            {
+                move = 0;
+            }
         }
-
-        if (Camera.main.orthographicSize == minZoom || Camera.main.orthographicSize == maxZoom)
+        else
         {
-            moving = 0;
-        }
-        if (cam.position == player.position || cam.position == levelCenter)
-        {
-            moving = 0;
+            move = 0;
         }
 
     }
 
+    /* Zooms camera to the desired level. Can be any float value, but this uses only two. */
     void Zoom(float target)
     {
         float zoom = Mathf.MoveTowards(Camera.main.orthographicSize, target, zoomSpeed * Time.deltaTime);
@@ -66,6 +75,8 @@ public class CameraZoom : MonoBehaviour
         Camera.main.orthographicSize = zoom;
     }
 
+    /* Pans the camera to the given position. Really is a Vector3, but
+     splitting them up allows for any offsets to be added prior to the Move call.*/
     void Move(float x, float y, float z)
     {
         float dx = Mathf.MoveTowards(cam.position.x, x, zoomSpeed * Time.deltaTime);
@@ -74,9 +85,17 @@ public class CameraZoom : MonoBehaviour
         cam.position = new Vector3(dx, dy, z);
     }
 
-    static void TriggerEvent(Vector3 centerPoint) {
+    /* Static callable function to manually pan the camera to a specific location. */
+    static void StartEvent(Vector3 centerPoint)
+    {
         eventCenter = centerPoint;
-        moving = 8;
+        move = 8;
+    }
+
+    /* Returns the camera to the player; after a StartEvent. */
+    static void StopEvent()
+    {
+        move = -1;
     }
 
 }
