52b43948b03901f95d7634b8a44b6705b0a4cf3e
will-shaw <will_shaw@live.com>
2017-01-30 22:45:14 +1300

    Added initial main_menu scene and facility intro scene. Added various textures.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform bg;
+    public Transform player;
+    public float speed = 2.0f;
+    float cooldown = 0;
+
+    void Update()
+    {
+		if (Input.GetKey(KeyCode.D)) {
+			player.GetComponent<Rigidbody2D>().AddForce(new Vector2(Time.deltaTime * 250f, 0));			
+		} else if (Input.GetKey(KeyCode.A)) {
+			player.GetComponent<Rigidbody2D>().AddForce(new Vector2(Time.deltaTime * -250f, 0));						
+		}
+
+        if (Input.GetKey(KeyCode.W) && Time.realtimeSinceStartup > cooldown + 1)
+        {
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 250.0f));
+            cooldown = Time.realtimeSinceStartup;
+        }
+    }
+}
