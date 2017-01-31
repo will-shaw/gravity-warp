de1f795a61135537b6c48c5f86f8221ecbd3a815
Daniel Thomson <thoda061@student.otago.ac.nz>
2017-01-31 16:36:37 +1300

    Added button scripts.

---
+++ Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleButton : MonoBehaviour {
+	bool on =false;
+
+	public Transform[] buttonItems;
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(on){
+			on = false;
+			Debug.Log(false);
+			toggleItemStatus();
+		}else{
+			on = true;
+			Debug.Log(true);
+			toggleItemStatus();
+		}
+	}
+
+	void toggleItemStatus() {
+		foreach(Transform item in buttonItems) {
+			if(item.GetComponent<Laser>() != null) {
+				item.gameObject.GetComponent<Laser>().ButtonPressed();
+			}
+		}
+	}
+}
