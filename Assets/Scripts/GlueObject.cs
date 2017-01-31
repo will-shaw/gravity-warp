e913680a77f9cbddfc69df030f2cff4ed9acd3a8
will-shaw <will_shaw@live.com>
2017-02-01 12:43:07 +1300

    Updated button and glue prefabs. Made glue script non-static (interferes with colliders).

--- Assets/Scripts/GlueObject.cs
+++ Assets/Scripts/GlueObject.cs
@@ -1,17 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class GlueObject : MonoBehaviour {
+public class GlueObject : MonoBehaviour
+{
 
-	void OnTriggerEnter2D(Collider2D other) {
-		if(other.gameObject.tag != "Wall") {
-			other.GetComponent<Glue>().gluing();
-			Destroy(gameObject);
-		} else {
-			GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-		}
-		
-	}
-		
+    public Sprite[] sprites = new Sprite[3];
+
+	bool stuck, once;
+
+    string currGrav;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag != "Wall")
+        {
+            other.GetComponent<Glue>().gluing();
+            GetComponent<SpriteRenderer>().sprite = sprites[2];
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+			stuck = true;			
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            GetComponent<SpriteRenderer>().sprite = sprites[1];
+			stuck = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag != "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (GravityWarp.gravity != currGrav && !stuck)
+        {
+			currGrav = GravityWarp.gravity;
+            if (GravityWarp.gravity == "U")
+            {
+                gameObject.transform.Rotate(new Vector3(0, 0, 0));
+            }
+            else if (GravityWarp.gravity == "R")
+            {
+                gameObject.transform.Rotate(new Vector3(0, 0, 90));
+            }
+            else if (GravityWarp.gravity == "D")
+            {
+                gameObject.transform.Rotate(new Vector3(0, 0, 180));
+            }
+            else if (GravityWarp.gravity == "L")
+            {
+                gameObject.transform.Rotate(new Vector3(0, 0, 270));
+            }
+        }
+    }
+
 }
