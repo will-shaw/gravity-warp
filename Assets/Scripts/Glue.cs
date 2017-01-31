792923340844f6eb8a62c27d0b4a702cd27f1306
Daniel Thomson <thoda061@student.otago.ac.nz>
2017-01-31 14:42:38 +1300

    Added GravityWarp, Glue and Laser scripts.

---
+++ Assets/Scripts/Glue.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glue : MonoBehaviour {
+
+	bool glued;
+
+	float gluedTime;
+
+	public float glueTime;
+
+
+	// Use this for initialization
+	void Start () {
+		glued  = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(glued && gluedTime > 0){
+			gluedTime -= Time.deltaTime;
+		} else if (glued) {
+			glued = false;
+			GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+		}
+		
+	}
+
+
+	public void gluing() {
+			glued = true;
+			gluedTime = glueTime;
+			GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+			Camera.main.GetComponent<GravityWarp>().changeGlueCount(0);
+	}
+
+
+	public bool isGlued (){
+		return glued;
+	}
+}
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+	public Sprite offSprite;
+
+	public Sprite onSprite;
+
+	public bool on;
+
+	public bool objectKilling;
+
+	public bool forceField;
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(on){
+			if(!(objectKilling) && !(forceField)) {
+				if(other.gameObject.tag == "Player") {
+					Destroy(other.gameObject);
+				}
+			} else if (objectKilling) {
+				if(other.gameObject.tag != "Wall") {
+					Destroy(other.gameObject);
+				}
+			}
+		}
+	}
+
+	public void ButtonPressed () {
+		if(on) {
+			on = false;
+			gameObject.GetComponent<SpriteRenderer>().sprite = offSprite;
+			if(forceField) {
+				gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+			}
+		} else {
+			on = true;
+			gameObject.GetComponent<SpriteRenderer>().sprite = onSprite;
+			if(forceField) {
+				gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+			}
+		}
+	}
+}
