24e20c69ae7b6681f8db90a4710c1ded14a918eb
will-shaw <will_shaw@live.com>
2017-02-01 12:43:25 +1300

    Added Door script.

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    int linkCount = 0;
+    public bool open = false;
+    public int linksRequired = 0;
+    public bool opensUpward = false;
+	public float openSpeed = 2;
+    float origin;
+	float offset = 3;
+
+    void Start()
+    {
+        origin = gameObject.transform.position.y;
+    }
+
+    public void Active(int source)
+    {
+        if (source == 1)
+        {
+            linkCount++;
+            UpdateState();
+        }
+        else
+        {
+            linkCount--;
+            UpdateState();
+        }
+    }
+
+    void UpdateState()
+    {
+        if (linkCount >= linksRequired)
+        {
+            open = true;
+        }
+        else
+        {
+            open = false;
+        }
+    }
+
+    void Update()
+    {
+        //if (gameObject.transform.position.y == origin && open)
+        //{
+            float y = Mathf.MoveTowards(gameObject.transform.position.y, origin + offset, openSpeed * Time.deltaTime);
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, y, gameObject.transform.position.z);
+        //}
+        //else if (open)
+       // {
+        //    float y = Mathf.MoveTowards(gameObject.transform.position.y, origin, openSpeed * Time.deltaTime);
+        //    gameObject.transform.position = new Vector3(gameObject.transform.position.x, y, gameObject.transform.position.z);
+        //}
+    }
+
+}
