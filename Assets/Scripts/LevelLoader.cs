975214bb0b411964771f26b8890fe18de289fb2b
will-shaw <will_shaw@live.com>
2017-02-02 03:29:45 +1300

    Added facility intro artwork. Updated LevelLoader script. Updated some image maximum resolutions. Updated the facility intro scene.

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,60 +1,38 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class LevelLoader : MonoBehaviour
 {
 
-	public Transform entry_point;
-	public Transform exit_point;
-	public Transform player;
+    public Transform entry_point;
+    public Transform exit_point;
+    public Transform switch_point;
+    public Transform player;
+    public Transform exitDoor;
+    public string nextLevel;
+    public bool asyncLoad = true;
+    bool once;
+    AsyncOperation op;
 
-	public static Vector2 playerPos;
-
-	public string nextLevel;
-
-	public string loadingScene;
-
-	bool inLoadArea = false;
-
-	public bool asyncLoad = true;
-
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		inLoadArea = true;
-	}
-
-    void OnTriggerExit2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "entry_point")
+        if (other.tag == "switch_point")
         {
-			if (asyncLoad) {
-				print("Loading " + nextLevel);
-				SceneManager.LoadSceneAsync(nextLevel).allowSceneActivation = true;
-			} else {
-				//SceneManager.LoadSceneAsync(loadingScene);
-			}
+            op.allowSceneActivation = true;
         }
         else if (other.tag == "exit_point")
         {
-			inLoadArea = false;
-			if (asyncLoad) {
-				print("Loading " + nextLevel);
-				SceneManager.LoadSceneAsync(nextLevel).allowSceneActivation = true;
-				
-			} else {
-				//SceneManager.LoadScene(loadingScene);
-			}
+            if (asyncLoad && !once)
+            {
+                once = true;
+                print("Loading " + nextLevel);
+                op = SceneManager.LoadSceneAsync(nextLevel);
+                op.allowSceneActivation = false;
+                exitDoor.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+                exitDoor.GetComponent<Rigidbody2D>().gravityScale = 4f;
+            }
         }
     }
-
-	void Start() {
-	//		player.position = new Vector2 (entry_point.position.x + playerPos.x, entry_point.position.y + playerPos.y);
-	}
-
-	void Update() {
-		if (inLoadArea) {
-			playerPos = new Vector2 (Mathf.Abs(player.position.x) - Mathf.Abs(exit_point.position.x), Mathf.Abs(player.position.y) - Mathf.Abs(exit_point.position.y));
-		}
-	}
 
 }
