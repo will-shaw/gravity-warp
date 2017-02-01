f3debe20a831204104f4beeb41197d15a5b2e7a6
Daniel Thomson <thoda061@student.otago.ac.nz>
2017-02-01 16:23:06 +1300

    Modified player so player movement doesn’t worked when glue and increased jump force so player can jump boxes.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,40 +9,42 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-		if (Input.GetKey(KeyCode.D)) {
-			//player.GetComponent<Rigidbody2D>().AddForce(new Vector2(Time.deltaTime * 250f, 0));
-            if(GravityWarp.gravity == "R") {
-                player.transform.Translate(new Vector3(0, Time.deltaTime * speed * -1,0), Space.World);
-            } else if (GravityWarp.gravity == "U" || GravityWarp.gravity == "D"){
-                player.transform.Translate(new Vector3(Time.deltaTime * speed,0,0), Space.World);
-            } else if (GravityWarp.gravity == "L"){
-                player.transform.Translate(new Vector3(0, Time.deltaTime * speed,0), Space.World);
-            } else if (GravityWarp.gravity == "U"){
-                player.transform.Translate(new Vector3(Time.deltaTime * speed,0,0), Space.World);
-            }		
-		} else if (Input.GetKey(KeyCode.A)) {
-			//player.GetComponent<Rigidbody2D>().AddForce(new Vector2(Time.deltaTime * -250f, 0));
-            if(GravityWarp.gravity == "R") {
-                player.transform.Translate(new Vector3(0, Time.deltaTime * speed,0), Space.World);
-            } else if (GravityWarp.gravity == "U" || GravityWarp.gravity == "D"){
-                player.transform.Translate(new Vector3(Time.deltaTime * speed * -1,0,0), Space.World);
-            } else if (GravityWarp.gravity == "L"){
-                player.transform.Translate(new Vector3(0, Time.deltaTime * speed * -1,0), Space.World);
-            }								
-		}
+        if(!(player.GetComponent<Glue>().isGlued())) {
+            if (Input.GetKey(KeyCode.D)) {
+                //player.GetComponent<Rigidbody2D>().AddForce(new Vector2(Time.deltaTime * 250f, 0));
+                if(GravityWarp.gravity == "R") {
+                    player.transform.Translate(new Vector3(0, Time.deltaTime * speed * -1,0), Space.World);
+                } else if (GravityWarp.gravity == "U" || GravityWarp.gravity == "D"){
+                    player.transform.Translate(new Vector3(Time.deltaTime * speed,0,0), Space.World);
+                } else if (GravityWarp.gravity == "L"){
+                    player.transform.Translate(new Vector3(0, Time.deltaTime * speed,0), Space.World);
+                } else if (GravityWarp.gravity == "U"){
+                    player.transform.Translate(new Vector3(Time.deltaTime * speed,0,0), Space.World);
+                }		
+            } else if (Input.GetKey(KeyCode.A)) {
+                //player.GetComponent<Rigidbody2D>().AddForce(new Vector2(Time.deltaTime * -250f, 0));
+                if(GravityWarp.gravity == "R") {
+                    player.transform.Translate(new Vector3(0, Time.deltaTime * speed,0), Space.World);
+                } else if (GravityWarp.gravity == "U" || GravityWarp.gravity == "D"){
+                    player.transform.Translate(new Vector3(Time.deltaTime * speed * -1,0,0), Space.World);
+                } else if (GravityWarp.gravity == "L"){
+                    player.transform.Translate(new Vector3(0, Time.deltaTime * speed * -1,0), Space.World);
+                }								
+            }
 
-        if (Input.GetKey(KeyCode.W) && Time.realtimeSinceStartup > cooldown + 1)
-        {
-            if(GravityWarp.gravity == "D") {
-                player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 500.0f));
-            } else if(GravityWarp.gravity == "U") {
-                player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -500.0f));
-            } else if(GravityWarp.gravity == "L") {
-                player.GetComponent<Rigidbody2D>().AddForce(new Vector2(-250.0f, 0));
-            } else if(GravityWarp.gravity == "R") {
-                player.GetComponent<Rigidbody2D>().AddForce(new Vector2(250.0f, 0));
-            } 
-            cooldown = Time.realtimeSinceStartup;
+            if (Input.GetKey(KeyCode.W) && Time.realtimeSinceStartup > cooldown + 1)
+            {
+                if(GravityWarp.gravity == "D") {
+                    player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 700.0f));
+                } else if(GravityWarp.gravity == "U") {
+                    player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -700.0f));
+                } else if(GravityWarp.gravity == "L") {
+                    player.GetComponent<Rigidbody2D>().AddForce(new Vector2(-500.0f, 0));
+                } else if(GravityWarp.gravity == "R") {
+                    player.GetComponent<Rigidbody2D>().AddForce(new Vector2(500.0f, 0));
+                } 
+                cooldown = Time.realtimeSinceStartup;
+            }
         }
     }
 }
