27c437a6863ee07b44c4338b66ed884d0d12ebc4
will-shaw <will_shaw@live.com>
2017-02-03 00:53:37 +1300

    Fixed z axis rotatation after becoming unglued. Made glue a green tint for easier viewing.

--- Assets/Scripts/Glue.cs
+++ Assets/Scripts/Glue.cs
@@ -1,41 +1,50 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-public class Glue : MonoBehaviour {
+public class Glue : MonoBehaviour
+{
 
-	bool glued;
-	float gluedTime;
-	public float glueTime;
-	
-	public Transform canvas;
-	void Start () {
-		glued  = false;
-		if(gameObject.GetComponent<Player>() != null){
-				canvas = gameObject.GetComponent<Player>().canvas;
-			}
-	}
-	
-	
-	void Update () {
-		if(glued && gluedTime > 0){
-			gluedTime -= Time.deltaTime;
-			if(gameObject.GetComponent<Player>() != null){
-				canvas.FindChild("Text").GetComponent<Text>().text = string.Format("{0:0}",gluedTime);
-			}
-		} else if (glued) {
-			glued = false;
-			GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;			
-		}
-		
-	}
+    bool glued;
+    float gluedTime;
+    public float glueTime;
 
-	public void gluing() {
-			glued = true;
-			gluedTime = glueTime;
-			GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
-			Camera.main.GetComponent<CameraZoom>().player.GetComponent<GlueControl>().changeGlueCount(0);
-	}
+    public Transform canvas;
+    void Start()
+    {
+        glued = false;
+        if (gameObject.GetComponent<Player>() != null)
+        {
+            canvas = gameObject.GetComponent<Player>().canvas;
+        }
+    }
 
-	public bool isGlued (){
-		return glued;
-	}
+    void Update()
+    {
+        if (glued && gluedTime > 0)
+        {
+            gluedTime -= Time.deltaTime;
+            if (gameObject.GetComponent<Player>() != null)
+            {
+                canvas.FindChild("Text").GetComponent<Text>().text = string.Format("{0:0}", gluedTime);
+            }
+        }
+        else if (glued)
+        {
+            glued = false;
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+
+    }
+
+    public void gluing()
+    {
+        glued = true;
+        gluedTime = glueTime;
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        Camera.main.GetComponent<CameraZoom>().player.GetComponent<GlueControl>().changeGlueCount(0);
+    }
+
+    public bool isGlued()
+    {
+        return glued;
+    }
 }
