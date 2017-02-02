16d26838c2e66b076d62174bb79961fa717789a9
Zachary Gardner <garza181@student.otago.ac.nz>
2017-02-02 16:17:19 +1300

    various ui updates

--- Assets/Scripts/Glue.cs
+++ Assets/Scripts/Glue.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
-
+using UnityEngine.UI;
 public class Glue : MonoBehaviour {
 
 	bool glued;
 	float gluedTime;
 	public float glueTime;
-
+	
+	public Transform canvas;
 	void Start () {
 		glued  = false;
+		if(gameObject.GetComponent<Player>() != null){
+				canvas = gameObject.GetComponent<Player>().canvas;
+			}
 	}
+	
 	
 	void Update () {
 		if(glued && gluedTime > 0){
 			gluedTime -= Time.deltaTime;
+			if(gameObject.GetComponent<Player>() != null){
+				canvas.FindChild("Text").GetComponent<Text>().text = string.Format("{0:N2}",gluedTime);
+			}
 		} else if (glued) {
 			glued = false;
 			GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;			
