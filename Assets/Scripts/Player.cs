1851d08f8fd08099f70feabad24200461ee4f5e8
will-shaw <will_shaw@live.com>
2017-02-04 20:36:04 +1300

    Added UI canvas prefab. Removed canvases from each level. Updated the player script to instantiate the canvas on the player. Various other updates. Player.cs : removed chained ifs, replaced with switch.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,20 +2,28 @@
 using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
-    public Transform bg;
     Transform player;
     public float speed = 10f;
     float cooldown = 0;
     public Rigidbody2D rb2D;
 
-    public RectTransform canvas;
+    public Transform canvas;
+    public RectTransform canvasPrefab;
+
     bool facingRight = true;
     Animator anim;
 
     public Sprite spSide;
-
     public Sprite spUp;
-    void Start() {
+
+    void Awake()
+    {
+        // Immediately Instantiate canvas so that Glue.cs can access it.
+        canvas = Instantiate(canvasPrefab, GameObject.Find("PlayerLevel").transform, true);
+    }
+
+    void Start()
+    {
         player = transform;
         rb2D = player.GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
@@ -23,51 +31,80 @@ public class Player : MonoBehaviour
         Camera.main.GetComponent<CameraZoom>().player = transform;
     }
 
-    float CalculateVelocity(float x, float y) {
+    float CalculateVelocity(float x, float y)
+    {
         return Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(y, 2));
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         float moveHori = Input.GetAxis("Horizontal");
         float moveVert = Input.GetAxis("Vertical");
         float cooldown = Camera.main.GetComponent<GravityWarp>().coolDown;
-        if(cooldown!=0f){
-            cooldown = 2-cooldown;
+        if (cooldown != 0f)
+        {
+            cooldown = 2 - cooldown;
         }
         //canvas.FindChild("Xtext").GetComponent<Text>().text = "V:" + string.Format("{0:N2}", CalculateVelocity(GetComponent<Rigidbody2D>().velocity.x, GetComponent<Rigidbody2D>().velocity.y));
-        
-        canvas.FindChild("Cooldown").GetComponent<Text>().text = "Cool Down: "+ string.Format("{0:N2}",cooldown);
-        canvas.FindChild("xtext").GetComponent<Text>().text = "X:" + string.Format("{0:N2}", GetComponent<Rigidbody2D>().velocity.x);
-        canvas.FindChild("ytext").GetComponent<Text>().text = "Y:" + string.Format("{0:N2}", GetComponent<Rigidbody2D>().velocity.y);
 
-        if(GravityWarp.gravity =="L" || GravityWarp.gravity == "R"){
-            anim.SetFloat("Speed", Mathf.Abs(moveVert));
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, moveVert * speed );
-            if (moveVert > 0 && !facingRight && GravityWarp.gravity == "R") {
-                Flip();
-            } else if (moveVert < 0 && facingRight && GravityWarp.gravity == "R") {
-                Flip();
-            } else if (moveVert > 0 && facingRight && GravityWarp.gravity == "L") {
-                Flip();
-            } else if (moveVert < 0 && !facingRight && GravityWarp.gravity == "L") {
+        canvas.GetChild(6).GetComponent<Text>().text = "Cool Down: " + string.Format("{0:N2}", cooldown);
+        canvas.GetChild(5).GetComponent<Text>().text = "X:" + string.Format("{0:N2}", GetComponent<Rigidbody2D>().velocity.x);
+        canvas.GetChild(4).GetComponent<Text>().text = "Y:" + string.Format("{0:N2}", GetComponent<Rigidbody2D>().velocity.y);
+
+        string gravity = GravityWarp.gravity;
+
+        if (gravity == "L" || gravity == "R")
+        {
+            if (!(player.GetComponent<Glue>().isGlued()))
+            {
+                anim.SetFloat("Speed", Mathf.Abs(moveVert));
+            }
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, moveVert * speed);
+            if (moveVert > 0 && !facingRight && gravity == "R")
+            {
                 Flip();
             }
-        }else {
-            anim.SetFloat("Speed", Mathf.Abs(moveHori));
+            else if (moveVert < 0 && facingRight && gravity == "R")
+            {
+                Flip();
+            }
+            else if (moveVert > 0 && facingRight && gravity == "L")
+            {
+                Flip();
+            }
+            else if (moveVert < 0 && !facingRight && gravity == "L")
+            {
+                Flip();
+            }
+        }
+        else
+        {
+            if (!(player.GetComponent<Glue>().isGlued()))
+            {
+                anim.SetFloat("Speed", Mathf.Abs(moveHori));
+            }
             GetComponent<Rigidbody2D>().velocity = new Vector2(moveHori * speed, GetComponent<Rigidbody2D>().velocity.y);
-            if (moveHori > 0 && !facingRight && GravityWarp.gravity == "D") {
+            if (moveHori > 0 && !facingRight && gravity == "D")
+            {
                 Flip();
-            } else if (moveHori < 0 && facingRight && GravityWarp.gravity == "D") {
+            }
+            else if (moveHori < 0 && facingRight && gravity == "D")
+            {
                 Flip();
-            }  else if (moveHori > 0 && facingRight && GravityWarp.gravity == "U") {
+            }
+            else if (moveHori > 0 && facingRight && gravity == "U")
+            {
                 Flip();
-            }  else if (moveHori < 0 && !facingRight && GravityWarp.gravity == "U") {
+            }
+            else if (moveHori < 0 && !facingRight && gravity == "U")
+            {
                 Flip();
             }
         }
     }
-    
-    void Flip() {
+
+    void Flip()
+    {
         facingRight = !facingRight;
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
@@ -75,111 +112,133 @@ public class Player : MonoBehaviour
     }
 
     void Update()
-    {   
+    {
+        canvas.position = transform.position;
 
-        Vector3 hello = gameObject.transform.position;
-        canvas.position = hello;
-
-        if (player != null) {
+        if (player != null)
+        {
             ChangeDirection();
+            IsGrounded();
         }
-        if(!(player.GetComponent<Glue>().isGlued())) {
-            if (Input.GetKey(KeyCode.Space) && Time.realtimeSinceStartup > cooldown + 1)
+        if (!(player.GetComponent<Glue>().isGlued()))
+        {
+            if (Input.GetKey(KeyCode.Space) && Time.realtimeSinceStartup > cooldown + 0.1f && IsGrounded())
             {
-                if(GravityWarp.gravity == "D") {
-                    player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 700.0f));
-                } else if(GravityWarp.gravity == "U") {
-                    player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -700.0f));
-                } else if(GravityWarp.gravity == "L") {
-                    player.GetComponent<Rigidbody2D>().AddForce(new Vector2(550.0f, 0));
-                } else if(GravityWarp.gravity == "R") {
-                    player.GetComponent<Rigidbody2D>().AddForce(new Vector2(-550.0f, 0));
-                } 
+                switch (GravityWarp.gravity)
+                {
+                    case "D":
+                        player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 700.0f));
+                        break;
+                    case "U":
+                        player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -700.0f));
+                        break;
+                    case "L":
+                        player.GetComponent<Rigidbody2D>().AddForce(new Vector2(500.0f, 0));
+                        break;
+                    case "R":
+                        player.GetComponent<Rigidbody2D>().AddForce(new Vector2(-500.0f, 0));
+                        break;
+                }
                 cooldown = Time.realtimeSinceStartup;
             }
         }
     }
 
-    void ChangeDirection() {
-        //string nearestWall = GetNearestWall();
-        if (/*nearestWall == "Down" && */GravityWarp.gravity == "D" && !(GetComponent<Glue>().isGlued())) {
-            //player.transform.rotation = new Quaternion(0,0,0,0);
-            player.transform.eulerAngles = new Vector3(0,0,0);
-        } else if (/*nearestWall == "Up" && */GravityWarp.gravity == "U" && !(GetComponent<Glue>().isGlued())) {
-            //player.transform.rotation = new Quaternion(0,0,180,0);
-            player.transform.eulerAngles = new Vector3(0,0,180);
-        } else if (/*nearestWall == "Left" && */GravityWarp.gravity == "L" && !(GetComponent<Glue>().isGlued())) {
-            //player.transform.rotation = new Quaternion(0,0,270,0);
-            player.transform.eulerAngles = new Vector3(0,0,270);
-        } else if (/*nearestWall == "Right" && */GravityWarp.gravity == "R" && !(GetComponent<Glue>().isGlued())) {
-            //player.transform.rotation = new Quaternion(0,0,-90,0);
-            player.transform.eulerAngles = new Vector3(0,0,90);
-        }  
-        Transform gravDirect = canvas.FindChild("GravityDirection");
-        if(GravityWarp.gravity == "R"){
-            gravDirect.GetComponent<SpriteRenderer>().sprite = spSide;
-            gravDirect.GetComponent<SpriteRenderer>().flipX = true;
-        }else if(GravityWarp.gravity == "U"){
-            gravDirect.GetComponent<SpriteRenderer>().flipY = false;
-            gravDirect.GetComponent<SpriteRenderer>().sprite = spUp;
-        }else if(GravityWarp.gravity == "D"){
-            gravDirect.GetComponent<SpriteRenderer>().flipY = true;
-            gravDirect.GetComponent<SpriteRenderer>().sprite = spUp;
-        }else if(GravityWarp.gravity == "L"){
-            gravDirect.GetComponent<SpriteRenderer>().sprite = spSide;
-            gravDirect.GetComponent<SpriteRenderer>().flipX = false;
+    /* Sets direction of player and floating UI indicator based on Gravity. */
+    void ChangeDirection()
+    {
+        if (!(GetComponent<Glue>().isGlued()))
+        {
+            switch (GravityWarp.gravity)
+            {
+                case "D":
+                    player.transform.eulerAngles = new Vector3(0, 0, 0);
+                    break;
+                case "U":
+                    player.transform.eulerAngles = new Vector3(0, 0, 180);
+                    break;
+                case "L":
+                    player.transform.eulerAngles = new Vector3(0, 0, 270);
+                    break;
+                case "R":
+                    player.transform.eulerAngles = new Vector3(0, 0, 90);
+                    break;
+            }
+        }
+
+        SpriteRenderer spr = canvas.GetChild(1).GetComponent<SpriteRenderer>();
+        switch (GravityWarp.gravity)
+        {
+            case "D":
+                spr.flipY = true;
+                spr.sprite = spUp;
+                break;
+            case "U":
+                spr.flipY = false;
+                spr.sprite = spUp;
+                break;
+            case "L":
+                spr.sprite = spSide;
+                spr.flipX = false;
+                break;
+            case "R":
+                spr.sprite = spSide;
+                spr.flipX = true;
+                break;
         }
     }
 
-    string GetNearestWall() {
-        RaycastHit2D[] down = Physics2D.RaycastAll(rb2D.position, Vector2.down, 6.0f);
-        RaycastHit2D[] up = Physics2D.RaycastAll(rb2D.position, Vector2.up, 6.0f);
-        RaycastHit2D[] left = Physics2D.RaycastAll(rb2D.position, Vector2.left, 6.0f);
-        RaycastHit2D[] right = Physics2D.RaycastAll(rb2D.position, Vector2.right, 6.0f);
+    /* Checks how far the player is from the floor. (Object must be tagged with 'Wall').
+        Fields & Buttons are detected too */
+    bool IsGrounded()
+    {
+        Vector2 down = Vector2.down;
+        string grav = GravityWarp.gravity;
+        switch (grav)
+        {
+            case "U":
+                down = Vector2.up;
+                break;
+            case "L":
+                down = Vector2.left;
+                break;
+            case "R":
+                down = Vector2.right;
+                break;
+        }
 
-        Debug.DrawRay(transform.position, Vector2.down, Color.red, 10);
-        Debug.DrawRay(transform.position, Vector2.up, Color.red, 10);
-        Debug.DrawRay(transform.position, Vector2.left, Color.yellow, 10);
-        Debug.DrawRay(transform.position, Vector2.right, Color.yellow, 10);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, down, 3.0f);
+        Debug.DrawRay(transform.position, down, Color.red, 10);
 
         float dDist = 100f;
-        float uDist = 100f;
-        float lDist = 100f;
-        float rDist = 100f;
 
-        foreach(RaycastHit2D hit in down) {
-            if (hit.collider != null && hit.collider.gameObject.tag == "Wall") {
-                dDist = Mathf.Abs(hit.point.y - rb2D.position.y);
-                Debug.DrawLine(transform.position, hit.point, Color.green, 10);
-            }
-        }
-        foreach(RaycastHit2D hit in up) {
-            if (hit.collider != null && hit.collider.gameObject.tag == "Wall") {
-                uDist = Mathf.Abs(hit.point.y - rb2D.position.y);
-                Debug.DrawLine(transform.position, hit.point, Color.green, 10);
-            }
-        }
-        foreach(RaycastHit2D hit in left) {
-            if (hit.collider != null && hit.collider.gameObject.tag == "Wall") {
-                lDist = Mathf.Abs(hit.point.y - rb2D.position.x);
-                Debug.DrawLine(transform.position, hit.point, Color.blue, 10);
-            }
-        }
-        foreach(RaycastHit2D hit in right) {
-            if (hit.collider != null && hit.collider.gameObject.tag == "Wall") {
-                rDist = Mathf.Abs(hit.point.y - rb2D.position.y);
-                Debug.DrawLine(transform.position, hit.point, Color.blue, 10);
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.tag == "Wall" || hit.collider.GetComponent<Field>() != null || hit.collider.GetComponent<Button>() != null)
+                {
+                    switch (grav)
+                    {
+                        case "U":
+                        case "D":
+                            dDist = Mathf.Abs(hit.point.y - transform.position.y);
+                            break;
+                        case "L":
+                        case "R":
+                            dDist = Mathf.Abs(hit.point.x - transform.position.x);
+                            break;
+                    }
+                    Debug.DrawLine(transform.position, hit.point, Color.green, 10);
+                }
             }
         }
 
-        float smallest = Mathf.Min(dDist, uDist, lDist, rDist);
-
-        if (smallest == dDist && dDist != 100f) return "Down";
-        if (smallest == uDist && uDist != 100f) return "Up";
-        if (smallest == lDist && lDist != 100f) return "Left";
-        if (smallest == rDist && rDist != 100f) return "Right";
-
-        return "Floating";
+        if (dDist <= 1.7f)
+        {
+            return true;
+        }
+        return false;
     }
 
 }
--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -5,6 +5,7 @@ public class Spike : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D other) {
 		if(other.gameObject.tag == "Player") {
 			Camera.main.GetComponent<GravityWarp>().playerDead = true;
+			Destroy (other.gameObject.GetComponent<Player>().canvas.gameObject);
 			Destroy(other.gameObject);
 		}
 	}
