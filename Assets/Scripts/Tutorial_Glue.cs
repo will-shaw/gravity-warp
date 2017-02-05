71167b7e0c6e0aaa85c1487d442b05036282287c
will-shaw <will_shaw@live.com>
2017-02-05 19:22:17 +1300

    Added short glue introduction scene + script and art. Added the scene to Build Settings.

---
+++ Assets/Scripts/Tutorial_Glue.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Tutorial_Glue : MonoBehaviour
+{
+
+    public Transform spawner;
+    public Transform gluePrefab;
+    public float glueSpawnDelay;
+    float glueSpawnTimer;
+
+    void Update()
+    {
+        glueSpawnTimer -= Time.deltaTime;
+        if (glueSpawnTimer <= 0)
+        {
+            Transform glue = Instantiate(gluePrefab, spawner.position, Quaternion.Euler(Vector2.up));
+            glue.GetComponent<SpriteRenderer>().flipY = true;
+            glueSpawnTimer = glueSpawnDelay;
+        }
+    }
+}
