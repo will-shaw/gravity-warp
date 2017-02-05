9769cab1c0222ed0fccb4a319f16344651b3a6d7
will-shaw <will_shaw@live.com>
2017-02-05 23:49:56 +1300

    Added a location to pick up the glue gun in the glue tutorial. Added glue gun storage box art. Added glue gun pickup script.

---
+++ Assets/Scripts/GlueGunPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GlueGunPickup : MonoBehaviour {
+
+    public Sprite opened;
+    public AudioClip clip;
+
+    bool once;
+
+    void OnTriggerEnter2D(Collider2D other)
+	{
+		if(!once && other.gameObject.tag == "Player"){
+            other.gameObject.GetComponent<GlueControl>().glueEnabled = true;			
+            GetComponent<SpriteRenderer>().sprite = opened;
+            GetComponent<AudioSource>().PlayOneShot(clip, 1);
+            once = true;
+        }
+	}
+}
