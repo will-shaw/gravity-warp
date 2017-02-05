403dac0035c8402d95aff90ac082bec294598992
Daniel Thomson <thoda061@student.otago.ac.nz>
2017-02-06 12:07:14 +1300

    Made it so toggle and hold buttons are different colors. Also made it so button are pushed in when active.

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -5,12 +5,34 @@ public class Button : MonoBehaviour
     public Transform[] activates;
     public bool toggleable = true;
     public bool active = false;
+    //For depression, D is floor, U is roof and L/R are left and right wall.
+    public string wallAttached = "D";
+
+    void Start()
+    {
+        if (toggleable)
+        {
+            GetComponent<SpriteRenderer>().color = UnityEngine.Color.yellow;
+        }
+        else 
+        {
+            GetComponent<SpriteRenderer>().color = UnityEngine.Color.red;
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<GlueObject>() == null)
-        {
-            active = true;
+        {      
+            if (toggleable && active)
+            {
+                active = false;
+            }
+            else
+            {
+                active = true;
+            }
+            Depression();
 			Activate(1);
         }
     }
@@ -21,6 +43,7 @@ public class Button : MonoBehaviour
             if (other.gameObject.GetComponent<GlueObject>() == null)
             {
                 active = false;
+                Depression();
                 Activate(0);
             }
         }
@@ -38,5 +61,52 @@ public class Button : MonoBehaviour
             }
 		}
 	}
+
+    void Depression()
+    {
+        switch (wallAttached)
+        {
+            case "D":
+                if (active)
+                {
+                    transform.Translate(0, -0.4F, 0, 0);
+                }
+                else
+                {
+                    transform.Translate(0, 0.4F, 0, 0);
+                }
+                break;
+            case "U":
+                if (active)
+                {
+                    transform.Translate(0, 0.4F, 0, 0);
+                }
+                else
+                {
+                    transform.Translate(0, -0.4F, 0, 0);
+                }
+                break;
+            case "L":
+                if (active)
+                {
+                    transform.Translate(-0.4F, 0, 0, 0);
+                }
+                else
+                {
+                    transform.Translate(0.4F, 0, 0, 0);
+                }
+                break;
+            case "R":
+                if (active)
+                {
+                    transform.Translate(0.4F, 0, 0, 0);
+                }
+                else
+                {
+                    transform.Translate(-0.4F, 0, 0, 0);
+                }
+                break;
+        }
+    }
 
 }
