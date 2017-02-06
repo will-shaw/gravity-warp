230cb830a778d30b8f07cf67edabef91e94dd5cd
will-shaw <will_shaw@live.com>
2017-02-06 16:52:54 +1300

    Fixed bug ;)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -245,7 +245,7 @@ public class Player : MonoBehaviour
         {
             if (hit.collider != null)
             {
-                if (hit.collider.gameObject.tag == "Wall" || !hit.collider.GetComponent<Field>().laser || hit.collider.GetComponent<Button>() != null || hit.collider.GetComponent<BoxColision>() != null)
+                if (hit.collider.gameObject.tag == "Wall" || (hit.collider.GetComponent<Field>() && !hit.collider.GetComponent<Field>().laser) || hit.collider.GetComponent<Button>() != null || hit.collider.GetComponent<BoxColision>() != null)
                 {
                     switch (grav)
                     {
