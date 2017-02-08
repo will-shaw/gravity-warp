732938c2a99ed87185a1caa715e002daa4b78f23
will-shaw <will_shaw@live.com>
2017-02-09 03:37:16 +1300

    Added bgeinings of GameState container. Tweaked some code.

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+
+    static bool isPaused = false;
+    static bool hasGun = false;
+    static bool hasRemote = false;
+    static bool glueEnabled = false;
+    static bool gravityEnabled = false;
+
+    public static bool GlueEnabled()
+    {
+        return glueEnabled;
+    }
+
+    public static void GlueEnabled(bool b)
+    {
+        glueEnabled = b;
+    }
+
+    public static bool GravityEnabled()
+    {
+        return gravityEnabled;
+    }
+
+    public static void GravityEnabled(bool b)
+    {
+        gravityEnabled = b;
+    }
+
+    public static bool HasGun()
+    {
+        return hasGun;
+    }
+
+    public static void HasGun(bool b)
+    {
+        hasGun = b;
+    }
+
+    public static bool HasRemote()
+    {
+        return hasRemote;
+    }
+
+    public static void HasRemote(bool b)
+    {
+        hasRemote = b;
+    }
+
+    public static void Pause(bool b)
+    {
+        isPaused = b;
+    }
+
+    public static bool IsPaused()
+    {
+        return isPaused;
+    }
+
+
+}
--- Assets/Scripts/MouseControls.cs
+++ Assets/Scripts/MouseControls.cs
@@ -11,49 +11,52 @@ public class MouseControls : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(InputManager.gravityDown) && GetComponent<GravityWarp>().gravityControlEnabled)
+        if (InputManager.gravityControlScheme == 0)
         {
-            this.x = Input.mousePosition.x;
-            this.y = Input.mousePosition.y;
+            if (Input.GetKeyDown(InputManager.gravityDown) && GetComponent<GravityWarp>().gravityControlEnabled)
+            {
+                this.x = Input.mousePosition.x;
+                this.y = Input.mousePosition.y;
 
-            // Play a sound here. (Quietly).
-            // Instantiate the radial menu at the mouse position.
-            Vector3 mousePos = Input.mousePosition;
-            mousePos.z = 4.0f;
-            Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
-            instantiatedMenu = Instantiate(menu, objectPos, Quaternion.identity);
+                // Play a sound here. (Quietly).
+                // Instantiate the radial menu at the mouse position.
+                Vector3 mousePos = Input.mousePosition;
+                mousePos.z = 4.0f;
+                Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+                instantiatedMenu = Instantiate(menu, objectPos, Quaternion.identity);
+            }
+            if (Input.GetKeyUp(InputManager.gravityDown) && GetComponent<GravityWarp>().gravityControlEnabled)
+            {
+                float x = Input.mousePosition.x;
+                float y = Input.mousePosition.y;
+
+                float dx = this.x - x;
+                float dy = this.y - y;
+
+                if (dx > distance && (dy < Deadzone(dx, 0) && dy > Deadzone(dx, 1)))
+                {
+                    GravityWarp.gravity = "L";
+                }
+                else if (dx < -distance && (dy < Deadzone(dx, 1) && dy > Deadzone(dx, 0)))
+                {
+                    GravityWarp.gravity = "R";
+                }
+                else if (dy > distance && (dx < Deadzone(dy, 0) && dx > Deadzone(dy, 1)))
+                {
+                    GravityWarp.gravity = "D";
+                }
+                else if (dy < -distance && (dx < Deadzone(dy, 1) && dx > Deadzone(dy, 0)))
+                {
+                    GravityWarp.gravity = "U";
+                }
+
+                if (instantiatedMenu)
+                {
+                    Destroy(instantiatedMenu.gameObject);
+                }
+            }
         }
-        if (Input.GetKeyUp(InputManager.gravityDown) && GetComponent<GravityWarp>().gravityControlEnabled)
-        {
-            float x = Input.mousePosition.x;
-            float y = Input.mousePosition.y;
-
-            float dx = this.x - x;
-            float dy = this.y - y;
-
-            if (dx > distance && (dy < Deadzone(dx, 0) && dy > Deadzone(dx, 1)))
-            {
-                GravityWarp.gravity = "L";
-            }
-            else if (dx < -distance && (dy < Deadzone(dx, 1) && dy > Deadzone(dx, 0)))
-            {
-                GravityWarp.gravity = "R";
-            }
-            else if (dy > distance && (dx < Deadzone(dy, 0) && dx > Deadzone(dy, 1)))
-            {
-                GravityWarp.gravity = "D";
-            }
-            else if (dy < -distance && (dx < Deadzone(dy, 1) && dx > Deadzone(dy, 0)))
-            {
-                GravityWarp.gravity = "U";
-            }
-
-            if (instantiatedMenu)
-            {
-                Destroy(instantiatedMenu.gameObject);
-            }
-        }
-
+        
     }
 
     float Deadzone(float v, int s)
