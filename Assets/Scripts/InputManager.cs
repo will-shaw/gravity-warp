f4faa14816504a7a4c34e7ebd7a903d5eacbe33f
Will Shaw <will_shaw@live.com>
2017-02-08 14:15:13 +1300

    Updated Input Manger; and Level_1 decorations

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,34 +3,38 @@ using System;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
-public class InputManager : MonoBehaviour {
+public class InputManager : MonoBehaviour
+{
 
-	/* Sets Control Scheme */
-	public static int controlScheme = 0;
+    /* Sets Control Scheme */
+    public static int controlScheme = 0;
 
-	/* Basic Movement */
-	public static KeyCode axisUp = KeyCode.W;
-	public static KeyCode axisDown = KeyCode.S;
-	public static KeyCode axisLeft = KeyCode.A;
-	public static KeyCode axisRight = KeyCode.D;
-	public static KeyCode jump = KeyCode.Space;
+    /* Left Hand. */
+    public static KeyCode axisUp = KeyCode.W;
+    public static KeyCode axisDown = KeyCode.S;
+    public static KeyCode axisLeft = KeyCode.A;
+    public static KeyCode axisRight = KeyCode.D;
+    public static KeyCode jump = KeyCode.Space;
+    public static KeyCode zoom = KeyCode.Tab;
 
-	/* Actions */
-	public static KeyCode zoom = KeyCode.Tab;
-	public static KeyCode gravityUp = KeyCode.Mouse0;
-	public static KeyCode gravityDown = KeyCode.Mouse0;
-	public static KeyCode gravityLeft = KeyCode.Mouse0;
-	public static KeyCode gravityRight = KeyCode.Mouse0;
-	public static KeyCode glue = KeyCode.Mouse1;
+    /* Right Hand. */
+    public static KeyCode gravityUp = KeyCode.Mouse0;
+    public static KeyCode gravityDown = KeyCode.Mouse0;
+    public static KeyCode gravityLeft = KeyCode.Mouse0;
+    public static KeyCode gravityRight = KeyCode.Mouse0;
+    public static KeyCode glue = KeyCode.Mouse1;
 
-	/* Menus */
-	public static KeyCode menu = KeyCode.Escape;
+    /* Menus */
+    public static KeyCode menu = KeyCode.Escape;
 
-	void Start() {
+    void Start()
+    {
+        UpdateControls();
+        Load();
+    }
 
-	}
-
-	void Save() {
+    void Save()
+    {
         BinaryFormatter bf = new BinaryFormatter();
         FileStream file = File.Open(Application.persistentDataPath + "/player-settings.dat", FileMode.Create);
         ControlSettings data = new ControlSettings();
@@ -39,16 +43,20 @@ public class InputManager : MonoBehaviour {
         data.axisLeft = axisLeft;
         data.axisRight = axisRight;
 
-		data.jump = jump;
-		data.zoom = zoom;
-		data.gravity = gravityUp;
-		data.glue = glue;
+        data.jump = jump;
+        data.zoom = zoom;
+        data.gravityUp = gravityUp;
+        data.gravityDown = gravityDown;
+        data.gravityLeft = gravityLeft;
+        data.gravityRight = gravityRight;
+        data.glue = glue;
 
         bf.Serialize(file, data);
         file.Close();
-	}
+    }
 
-	void Load() {
+    void Load()
+    {
         if (File.Exists(Application.persistentDataPath + "/player-settings.dat"))
         {
             BinaryFormatter bf = new BinaryFormatter();
@@ -62,13 +70,36 @@ public class InputManager : MonoBehaviour {
 
             jump = data.jump;
             zoom = data.zoom;
-			gravityUp = data.gravity;
-			glue = data.glue;
+            gravityUp = data.gravityUp;
+            gravityDown = data.gravityDown;
+            gravityLeft = data.gravityLeft;
+            gravityRight = data.gravityRight;
+            glue = data.glue;
         }
-	}
+    }
+
+    void UpdateControls()
+    {
+        switch (controlScheme)
+        {
+            case 0:
+                gravityUp = KeyCode.Mouse0;
+                gravityDown = KeyCode.Mouse0;
+                gravityLeft = KeyCode.Mouse0;
+                gravityRight = KeyCode.Mouse0;
+                glue = KeyCode.Mouse1;
+                break;
+            case 1:
+                gravityUp = KeyCode.UpArrow;
+                gravityDown = KeyCode.DownArrow;
+                gravityLeft = KeyCode.LeftArrow;
+                gravityRight = KeyCode.RightArrow;
+                glue = KeyCode.LeftControl;
+                break;
+        }
+    }
 
 }
-
 
 [Serializable]
 class ControlSettings
@@ -80,6 +111,8 @@ class ControlSettings
     public KeyCode jump;
     public KeyCode zoom;
     public KeyCode glue;
-    public KeyCode gravity;
-
+    public KeyCode gravityUp;
+    public KeyCode gravityDown;
+    public KeyCode gravityLeft;
+    public KeyCode gravityRight;
 }
