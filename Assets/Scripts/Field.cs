13e815e86900581ad0f5088656e743e382b52ba3
will-shaw <will_shaw@live.com>
2017-02-09 10:57:36 +1300

    Fixed object killing laser not setting playerdead when walked into.

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -1,41 +1,58 @@
 ﻿using UnityEngine;
 
-public class Field : MonoBehaviour {
+public class Field : MonoBehaviour
+{
 
-	public bool laser = false;
-	public bool active = true;
-	public bool objectKilling = true;
-	public int linksRequired = 1;
+    public bool laser = false;
+    public bool active = true;
+    public bool objectKilling = true;
+    public int linksRequired = 1;
 
-	public bool startOff = false;
-	int currentLinks = 0;
+    public bool startOff = false;
+    int currentLinks = 0;
 
-	void Start () {
-		if (active) {
-			gameObject.GetComponent<Renderer>().enabled = true;
-		} else {
-			gameObject.GetComponent<Renderer>().enabled = false;
-			gameObject.GetComponent<Collider2D>().enabled = false;
-		}
-	}
-	
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (active && laser){
-			if(objectKilling) {
-				if(other.gameObject.tag != "Wall") {
-					Destroy(other.gameObject);
-				}
-			} else {
-				if(other.gameObject.tag == "Player") {
-					//Destroy(other.gameObject);
-					Camera.main.GetComponent<GravityWarp>().playerDead = true;
-				}
-			}
-		}
-	}
+    void Start()
+    {
+        if (active)
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+            gameObject.GetComponent<Collider2D>().enabled = false;
+        }
+    }
 
-	public void ActivateLink(int source)
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (active && laser)
+        {
+            if (objectKilling)
+            {
+                if (other.gameObject.tag != "Wall")
+                {
+                    if (other.gameObject.tag == "Player")
+                    {
+                        Camera.main.GetComponent<GravityWarp>().playerDead = true;
+                    }
+                    else
+                    {
+                        Destroy(other.gameObject);
+                    }
+                }
+            }
+            else
+            {
+                if (other.gameObject.tag == "Player")
+                {
+                    Camera.main.GetComponent<GravityWarp>().playerDead = true;
+                }
+            }
+        }
+    }
+
+    public void ActivateLink(int source)
     {
         if (source == 1)
         {
@@ -45,37 +62,43 @@ public class Field : MonoBehaviour {
         {
             currentLinks--;
         }
+        ToggleField();
 
-    	ToggleField();
-
-		Debug.Log(currentLinks);
+        Debug.Log(currentLinks);
     }
 
-	public void ToggleField() {
-		if(startOff)
-		{
-			if (!(active) && currentLinks >= linksRequired) {		
-				gameObject.GetComponent<Renderer>().enabled = true;
-				gameObject.GetComponent<Collider2D>().enabled = true;
-				active = !active;
-			} else if(active){
-				gameObject.GetComponent<Renderer>().enabled = false;
-				gameObject.GetComponent<Collider2D>().enabled = false;
-				active = !active;
-			}
-		}
-		else 
-		{
-			if (active && currentLinks >= linksRequired) {		
-				gameObject.GetComponent<Renderer>().enabled = false;
-				gameObject.GetComponent<Collider2D>().enabled = false;
-				active = !active;
-			} else if(!(active)){
-				gameObject.GetComponent<Renderer>().enabled = true;
-				gameObject.GetComponent<Collider2D>().enabled = true;
-				active = !active;
-			}
-		}
-	}
+    public void ToggleField()
+    {
+        if (startOff)
+        {
+            if (!(active) && currentLinks >= linksRequired)
+            {
+                gameObject.GetComponent<Renderer>().enabled = true;
+                gameObject.GetComponent<Collider2D>().enabled = true;
+                active = !active;
+            }
+            else if (active)
+            {
+                gameObject.GetComponent<Renderer>().enabled = false;
+                gameObject.GetComponent<Collider2D>().enabled = false;
+                active = !active;
+            }
+        }
+        else
+        {
+            if (active && currentLinks >= linksRequired)
+            {
+                gameObject.GetComponent<Renderer>().enabled = false;
+                gameObject.GetComponent<Collider2D>().enabled = false;
+                active = !active;
+            }
+            else if (!(active))
+            {
+                gameObject.GetComponent<Renderer>().enabled = true;
+                gameObject.GetComponent<Collider2D>().enabled = true;
+                active = !active;
+            }
+        }
+    }
 
 }
