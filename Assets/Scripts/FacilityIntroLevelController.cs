bdd09cb23e228752429af676a634e5ba74e2e516
Will Shaw <will_shaw@live.com>
2017-02-09 14:31:41 +1300

    Animated ship in facility intro level.

--- Assets/Scripts/FacilityIntroLevelController.cs
+++ Assets/Scripts/FacilityIntroLevelController.cs
@@ -4,16 +4,30 @@ public class FacilityIntroLevelController : MonoBehaviour
 {
     public float gravityChangeTime;
 
+    public float shipLandDelay = 2;
+
     float gravityChangeTimer;
+    float shipLandTimer;
+
+    bool playerActive;
 
     void Start()
     {
         gravityChangeTimer = gravityChangeTime;
+        shipLandTimer = shipLandDelay;
     }
 
     void Update()
     {
+        if (!playerActive) {
+            GetComponent<GravityWarp>().player.gameObject.SetActive(false);
+        }
         gravityChangeTimer -= Time.deltaTime;
+        shipLandTimer -= Time.deltaTime;
+        if (shipLandTimer <= 0) {
+            GetComponent<GravityWarp>().player.gameObject.SetActive(true);
+            playerActive = true;
+        }
         if (gravityChangeTimer <= 0)
         {
             GravityWarp.gravity = GravityWarp.gravity == "D" ? "U" : "D";
