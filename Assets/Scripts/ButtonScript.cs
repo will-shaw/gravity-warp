71212cf81d80ae80497882f0da386f897aeaa785
will-shaw <will_shaw@live.com>
2017-02-10 02:57:56 +1300

    Added new button prefab with linked children and animations. More reliable animation with less setup required. These buttons use a new ButtonScript to avoid conflict with UnityEngine.UI.Button

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Transform[] activates;
+    AudioClip release;
+    bool connection;
+
+    void Start()
+    {
+        release = Camera.main.GetComponent<AudioManager>().GetButton();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<GlueObject>() == null && other.tag != "Wall")
+        {
+            GetComponent<AudioSource>().Play();
+            GetComponent<Animator>().SetBool("Active", true);
+            AddLink();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<GlueObject>() == null && other.tag != "Wall")
+        {
+            GetComponent<Animator>().SetBool("Active", false);
+            GetComponent<AudioSource>().PlayOneShot(release, 1);
+            SubLink();
+        }
+    }
+
+    void AddLink()
+    {
+        foreach (Transform item in activates)
+        {
+            if (item.GetComponent<Door>() != null)
+            {
+                item.GetComponent<Door>().ActivateLink(1);
+            }
+            if (item.GetComponent<Field>() != null)
+            {
+                item.GetComponent<Field>().ActivateLink(1);
+            }
+            if (item.GetComponent<Emit>() != null)
+            {
+                item.GetComponent<Emit>().toggle();
+            }
+        }
+    }
+
+    void SubLink()
+    {
+        foreach (Transform item in activates)
+        {
+            if (item.GetComponent<Door>() != null)
+            {
+                item.GetComponent<Door>().ActivateLink(0);
+            }
+            if (item.GetComponent<Field>() != null)
+            {
+                item.GetComponent<Field>().ActivateLink(0);
+            }
+            if (item.GetComponent<Emit>() != null)
+            {
+                item.GetComponent<Emit>().toggle();
+            }
+        }
+    }
+
+}
