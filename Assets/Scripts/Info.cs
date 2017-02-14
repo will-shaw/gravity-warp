a0120485e12b07a07b1936c99e710c344d334e17
Zachary Gardner <garza181@student.otago.ac.nz>
2017-02-15 12:24:06 +1300

    changed respawn z axis accordingly and removed the dialog box for gravity controls showing up on each load, only on first play through now.

--- Assets/Scripts/Info.cs
+++ Assets/Scripts/Info.cs
@@ -5,6 +5,8 @@ public class Info :MonoBehaviour{
 	public static Vector3 checkpoint;
 	public static bool load = false;
 	public static float checktime;
+
+	public static bool GravInfo = true;
 	
 	// Use this for initialization
 }
