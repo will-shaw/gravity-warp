e33032d8fab0f48d679470218ba058d00637f664
will-shaw <will_shaw@live.com>
2017-02-17 15:50:19 +1300

    Added final level.

---
+++ Assets/Scripts/GenArms.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GenArms : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Camera.main.GetComponent<GravityWarp>().playerDead = true;
+            other.gameObject.GetComponent<AudioSource>().PlayOneShot(Camera.main.GetComponent<AudioManager>().GetBoxCrush(), 1);
+            other.gameObject.GetComponent<Animator>().SetBool("isCrushed", true);
+        }
+    }
+
+}
