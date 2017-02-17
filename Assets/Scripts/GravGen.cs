9dcc7c68475d32b6d1e0128cbcf979ea4ba9a30f
will-shaw <will_shaw@live.com>
2017-02-17 17:11:29 +1300

    Updated end game

--- Assets/Scripts/GravGen.cs
+++ Assets/Scripts/GravGen.cs
@@ -7,6 +7,8 @@ public class GravGen : MonoBehaviour
 
     public Transform door;
 
+    bool once;
+
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -14,14 +16,32 @@ public class GravGen : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag != "Player")
+        if (other.tag != "Player" && !once)
         {
             anim.SetBool("explode", true);
             GetComponent<AudioSource>().Play();
             door.GetComponent<Door>().ActivateLink(1);
             GravityWarp.gravity = "D";
             Camera.main.GetComponent<GravityWarp>().gravityControlEnabled = false;
+            once = true;
         }
-
     }
+
+    void FixedUpdate() {
+        switch(GravityWarp.gravity) {
+            case "U":
+                anim.SetInteger("gravity", 1);
+                break;
+            case "D":
+                anim.SetInteger("gravity", 0);
+                break;
+            case "L":
+                anim.SetInteger("gravity", 2);
+                break;
+            case "R":
+                anim.SetInteger("gravity", 3);
+                break;
+        }
+    }
+
 }
